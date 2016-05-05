ac8fdeece01421376d45190c23e831fc298bac54
延强赵 <silvia_01@yanqiangzhaodeiMac.local>
2016-05-05 14:45:30 +0800

    haf

--- Exercise/Exercise/ViewController.swift
+++ Exercise/Exercise/ViewController.swift
@@ -8,28 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
-
+class ViewController: UIViewController, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource {
+    private let reuseIdentifier = "Cell"
+     var collection : UICollectionView?
+    var tag = 1
+    
     @IBOutlet weak var leftBtn: UIButton!
+    @IBAction func Live(sender: AnyObject) {
+        if tag == 1 {
+            self.view.addSubview(collection!)
+         tag = 2
+        }else{
+            collection?.removeFromSuperview()
+            tag = 1
+        }
+        
+        
+    }
     
-    
+   
+       
     @IBOutlet weak var rightBtn: UIButton!
     
     
     
+    
+    
     @IBOutlet weak var tableView: UITableView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         let cellNib: UINib = UINib(nibName: "DynamicTableViewCell", bundle: nil)
         self.tableView.registerNib(cellNib, forCellReuseIdentifier: "cell")
         self.tableView.rowHeight = 224
         
-        
-        
-        
-        
+        self.title = "同城"
+        let layout = UICollectionViewFlowLayout.init()
+        layout.itemSize = CGSize(width: 200, height: 300)
+        layout.scrollDirection = .Vertical
+       collection = UICollectionView(frame: self.tableView.frame, collectionViewLayout: layout)
+       
+        collection!.delegate = self
+        collection!.dataSource = self
+        collection!.registerNib(UINib(nibName: "LiveCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
+
+        rightBtn.tag = 1
         // Do any additional setup after loading the view, typically from a nib.
     }
+
+    
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return 20
     }
@@ -40,6 +68,27 @@ class ViewController: UIViewController, UITableViewDataSource {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as? DynamicTableViewCell
         return cell!
         
+    }
+    
+    
+    
+     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 20
+    }
+    
+     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as!LiveCollectionViewCell
+        
+        // Configure the cell
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
