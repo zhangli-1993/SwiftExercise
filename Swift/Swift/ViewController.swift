9d3b95657ca73305b47da174db17279aae47026a
延强赵 <silvia_02@yanqiangzhaodeiMac-2.local>
2016-05-05 08:56:09 +0800

    program

---
+++ Swift/Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift
+//
+//  Created by 延强赵 on 16/5/5.
+//  Copyright © 2016年 延强赵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
