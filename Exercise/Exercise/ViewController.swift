4b40582c7d6ece4900aecad3e9fa07e02ea856ab
延强赵 <silvia_01@yanqiangzhaodeiMac.local>
2016-05-05 09:19:03 +0800

    123

---
+++ Exercise/Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise
+//
+//  Created by 延强赵 on 16/5/5.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
